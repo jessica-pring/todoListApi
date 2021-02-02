2b840781c9484ca35c2aa5b4c827a8c0e657edab
Jess Pring <jessicapring@outlook.com>
2021-02-02 22:26:58 +0000

    initial commit

---
+++ api/controllers/todoListController.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const mongoose = require('mongoose'),
+  Task = mongoose.model('Tasks');
+
+exports.list_all_tasks = (req, res) => {
+  Task.find({}, =(err, task) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(task);
+  });
+}
+
+// exports.list_all_tasks = function (req, res) {
+//   Task.find({}, function (err, task) {
+//     if (err)
+//       res.send(err);
+//     res.json(task);
+//   });
+// };
+
+// exports.create_a_task = (req, res) => {
+//   const new_task = new Task(req.body);
+//   new_task.save()
+// }
